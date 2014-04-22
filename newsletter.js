659e12c7b760192c4cb3e89d07e2e8e9df929169
Constantine Kiriaze <ckiriaze@gmail.com>
2014-04-22 09:59:55 -0700

    init push

---
+++ newsletter.js
@@ -0,0 +1,39 @@
+(function($) {
+
+	$('#mc-subscribe-form').submit(function() {
+		
+	    var $this = $(this),
+	    	response = $('#response').hide();
+
+	    if ( $this.valid() ) {
+
+	        $.ajax({
+	            url: adminAjax.ajaxurl,
+	            type: 'POST',
+	            data: {
+	                'email' : $('#mce-email').val(),
+	                'action': 'newsletter_ajax_request',
+	                'nonce' : adminAjax.nonce
+	            },
+	            beforeSend: function(){
+
+	            },
+	            success: function(data){
+	                console.log(data);
+	                // console.log('success');
+	                $this.fadeTo(300,0).slideUp( 500, function(){
+                        response.html(data).slideDown().fadeTo(300,1);
+                    });
+	            },
+	            error: function(errorThrown) {
+	                console.log(errorThrown);
+	                // console.log('failed');
+	            }
+	        });
+
+	    }
+
+	    return false;
+	})
+
+})(jQuery)
