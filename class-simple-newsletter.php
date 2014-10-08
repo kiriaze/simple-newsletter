9060eb40a974ae763e2f421e19723c535201c21a
Constantine Kiriaze <ckiriaze@gmail.com>
2014-10-08 16:39:38 -0700

    updated for datacenters

--- class-simple-newsletter.php
+++ class-simple-newsletter.php
@@ -15,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
 
-    
+
     // ADD FUNTION TO WIDGETS_INIT
     add_action( 'widgets_init', 'simple_newsletter' );
 
@@ -35,8 +35,8 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
             parent::__construct(
                 'simple_newsletter',
                 'Newsletter (Simple)',
-                array( 
-                    'description' => __( 'A custom widget that displays a newsletter subscribe field', 'simple' ), 
+                array(
+                    'description' => __( 'A custom widget that displays a newsletter subscribe field', 'simple' ),
                 )
             );
 
@@ -51,10 +51,10 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
 
             // The $_REQUEST contains all the data sent via ajax
             if ( isset($_REQUEST) ) {
-                 
+
                 // If you're debugging, it might be useful to see what was sent in the $_REQUEST
                 // sp($_REQUEST);
-               
+
                 $apiKey         = $settings['2']['apiKey'];
                 $listId         = $settings['2']['listID'];
                 $double_optin   = false;
@@ -63,7 +63,10 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
                 $email          = $_POST['email'];
 
                 //replace us2 with your actual datacenter
-                $submit_url = "http://us3.api.mailchimp.com/1.3/?method=listSubscribe";
+                // grab the api key, and the last part after hyphen is usually the correct data center
+                $dataCenter = substr( $apiKey, strrpos( $apiKey, '-' ) + 1 );
+                $submit_url = "http://".$dataCenter.".api.mailchimp.com/1.3/?method=listSubscribe";
+
                 $data = array(
                     'email_address' => $email,
                     'apikey'        => $apiKey,
@@ -74,13 +77,13 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
                 );
 
                 $payload = json_encode($data);
-                 
+
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $submit_url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                 curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, urlencode($payload));
-                 
+
                 $result = curl_exec($ch);
                 curl_close ($ch);
                 $data = json_decode($result);
@@ -91,7 +94,7 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
                 }
 
             }
-             
+
             // Always die in functions echoing ajax content
            die();
         }
@@ -100,15 +103,15 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
         /*  DISPLAY WIDGET
         /*--------------------------------------------------------------------*/
         function widget( $args, $instance ) {
-            
-            wp_enqueue_script( 
-                'widget-newsletter', 
+
+            wp_enqueue_script(
+                'widget-newsletter',
                 plugins_url( 'newsletter.js', __FILE__ ),
-                array('jquery') 
+                array('jquery')
             );
 
             extract( $args );
-            
+
             $title = apply_filters('widget_title', $instance['title'] );
 
             // widget vars
@@ -118,24 +121,24 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
             // before widget
             echo $before_widget;
 
-            ?> 
-                
+            ?>
+
             <?php if ( $title ) echo $before_title . $title . $after_title; ?>
-            
+
                 <?php if ( $desc != '' ) : ?>
                     <p><?php echo $desc; ?></p>
                 <?php endif; ?>
 
                 <form action="" method="post" id="mc-subscribe-form" class="validate">
-                
+
                     <input type="email" name="email" class="email-newsletter" id="mce-email" placeholder="<?php echo $placeholder; ?>" required data-validate="validate(required, email)">
-                    
+
                     <input type="submit" value="<?php _e('Subscribe','simple'); ?>" class="btn large">
-                
+
                 </form>
 
                 <p id="response"></p>
-            
+
             <?php
 
             // after widget
@@ -147,9 +150,9 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
         /*  UPDATE WIDGET
         /*--------------------------------------------------------------------*/
         function update( $new_instance, $old_instance ) {
-            
+
             $instance = $old_instance;
-            
+
             // STRIP TAGS TO REMOVE HTML - IMPORTANT FOR TEXT IMPUTS
             $instance['title']          = strip_tags( $new_instance['title'] );
             $instance['apiKey']         = stripslashes( $new_instance['apiKey'] );
@@ -163,7 +166,7 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
 
         /*--------------------------------------------------------------------*/
         /*  WIDGET SETTINGS (FRONT END PANEL)
-        /*--------------------------------------------------------------------*/ 
+        /*--------------------------------------------------------------------*/
         function form( $instance ) {
 
             // WIDGET DEFAULTS
@@ -174,24 +177,24 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
                 'listID'        => '',
                 'desc'          => 'This is a nice and simple  email newsletter widget.'
             );
-                
+
             $instance = wp_parse_args( (array) $instance, $defaults ); ?>
-            
+
             <p>
                 <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title / Intro:', 'simple') ?></label>
                 <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" />
             </p>
-            
-            
+
+
             <p>
                 <textarea class="widefat" rows="5" cols="15" id="<?php echo $this->get_field_id( 'desc' ); ?>" name="<?php echo $this->get_field_name( 'desc' ); ?>"><?php echo $instance['desc']; ?></textarea>
             </p>
-            
+
             <p>
                 <label for="<?php echo $this->get_field_id( 'placeholder' ); ?>"><?php _e('Placeholder Text:', 'simple') ?></label>
                 <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'placeholder' ); ?>" name="<?php echo $this->get_field_name( 'placeholder' ); ?>" value="<?php echo $instance['placeholder']; ?>" />
             </p>
-            
+
             <p>
                 <label for="<?php echo $this->get_field_id( 'apiKey' ); ?>"><?php _e('API Key:', 'simple') ?> (<a href="https://us3.admin.mailchimp.com/account/api/">API Key</a>)</label>
                 <input type="text" class="widefat" id="<?php echo $this->get_field_id( 'apiKey' ); ?>" name="<?php echo $this->get_field_name( 'apiKey' ); ?>" value="<?php echo $instance['apiKey']; ?>" />
@@ -212,29 +215,33 @@ if ( ! class_exists( 'Simple_Newsletter_Widget' ) ) :
     if ( !function_exists('simple_newsletter_shortcode') ) {
 
         function simple_newsletter_shortcode( $atts ) {
-            extract(shortcode_atts(array(
+
+            extract( shortcode_atts( array(
                 'before'    => '',
                 'after'     => '',
                 'wrapper'   => 'div',
                 'class'     => '',
-            ), $atts));
-            
+            ), $atts ) );
+
             $output = '';
 
             $output .= $before;
 
             $output .= '<'.$wrapper.' class="'.$class.'">';
 
-            $output .= Simple_Newsletter_Widget::display_cached_content();
+            // $output .= new Simple_Newsletter_Widget; // need to find a way to repurpose code above for shortcode, maybe even plugin settings
 
             $output .= '</'.$wrapper.'>';
 
             $output .= $after;
 
             return $output;
+
         }
+
         add_shortcode('newsletter', 'simple_newsletter_shortcode');
 
     }
+
 
 endif;
